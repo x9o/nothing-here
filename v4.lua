1cb7d353136f3586997faef0c057c36a2671a00c
x9o <95175948+x9o@users.noreply.github.com>
2022-11-24 21:05:47 +0800

    Update v4.lua

--- v4.lua
+++ v4.lua
@@ -2144,31 +2144,6 @@ local SecondButton = FunstuffPage.AddButton("Super Jump (H to toggle, 'Time eras
         end
     end)	
 end)
-local SecondButton = FunstuffPage.AddButton("Fake macro (C)", function()
-   plr = game:GetService('Players').LocalPlayer
-        down = true
-         
-        function onButton1Down(mouse)
-            down = true
-            while down do
-                if not down then break end
-                local char = plr.Character
-                char.HumanoidRootPart.Velocity = char.HumanoidRootPart.CFrame.lookVector * 190
-                wait()
-            end
-        end
-         
-        function onButton1Up(mouse)
-            down = false
-        end
-         
-        function onSelected(mouse)
-            mouse.KeyDown:connect(function(c) if c:lower()=="c"then onButton1Down(mouse)end end)
-            mouse.KeyUp:connect(function(c) if c:lower()=="c"then onButton1Up(mouse)end end)
-        end
-        onSelected(game.Players.LocalPlayer:GetMouse())
-    end
-)
 
 ---------------
 for i,v in pairs(game:GetService("Workspace").Ignored.Shop:GetChildren()) do
