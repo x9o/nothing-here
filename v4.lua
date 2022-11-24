6eb6267d8db66cb391aa3e8990c4941d4b19bcb2
x9o <95175948+x9o@users.noreply.github.com>
2022-11-24 21:08:44 +0800

    Update v4.lua

--- v4.lua
+++ v4.lua
@@ -871,6 +871,32 @@ local SecondButton = AimlockPage.AddButton("Aimlock (Q)", function()
 end)
 
 -- funtab 
+local SecondButton = FunstuffPage.AddButton("Fake macro (C)", function()
+   plr = game:GetService('Players').LocalPlayer
+        down = true
+         
+        function onButton1Down(mouse)
+            down = true
+            while down do
+                if not down then break end
+                local char = plr.Character
+                char.HumanoidRootPart.Velocity = char.HumanoidRootPart.CFrame.lookVector * 190
+                wait()
+            end
+        end
+         
+        function onButton1Up(mouse)
+            down = false
+        end
+         
+        function onSelected(mouse)
+            mouse.KeyDown:connect(function(c) if c:lower()=="c"then onButton1Down(mouse)end end)
+            mouse.KeyUp:connect(function(c) if c:lower()=="c"then onButton1Up(mouse)end end)
+        end
+        onSelected(game.Players.LocalPlayer:GetMouse())
+    end
+)
+
 local fucknigger = FunstuffPage.AddButton("FOV [PRO]", function()
     workspace.CurrentCamera.FieldOfView = 120
 end)
