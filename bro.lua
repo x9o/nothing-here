ebd8923893b7fb7f71a6fad09beb71c3497d2143
x9o <95175948+x9o@users.noreply.github.com>
2022-12-02 11:34:45 +0800

    Update bro.lua

--- bro.lua
+++ bro.lua
@@ -2,7 +2,6 @@ local UILibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/blo
 
 local MainUI = UILibrary.Load("xoloware")
 local MainPage = MainUI.AddPage("Main")
-local CombatPage = MainUI.Addpage("Other Shit")
 -- // TOGGLE
 
 local Mouse = game.Players.LocalPlayer:GetMouse()
