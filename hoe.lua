913d7d1c0b3f313e8db25d6617518a5ff7983711
x9o <95175948+x9o@users.noreply.github.com>
2023-05-30 14:21:35 +0800

    Create hoe.lua

---
+++ hoe.lua
@@ -0,0 +1,21 @@
+local key = "y" -- the toxic key
+_G.Toxic = false
+
+local words = 
+	{
+		"МОТНЕRFUСКER",
+		"CОCК",
+  }
+
+local player = game:GetService("Players").LocalPlayer
+local mouse = player:GetMouse()
+
+local event = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+
+mouse.KeyDown:Connect(function(keybind)
+	if _G.Toxic == false then
+		if keybind == key then
+			event:FireServer(words[math.random(#words)], "All")
+		end
+	end
+end)
