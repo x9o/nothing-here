167921a9518b7131f3832c8145f681e464212d4a
x9o <95175948+x9o@users.noreply.github.com>
2023-06-16 15:00:07 +0800

    Create followbot.lua

---
+++ followbot.lua
@@ -0,0 +1,22 @@
+local key = "y" -- the toxic key
+_G.Toxic = false
+
+local words = 
+	{
+		"⚠️ Follow Bot ⚠️ (Beta),
+		"Type !stop to stop following.",
+		"Type !target <name> to follow another target.",
+  }
+
+local player = game:GetService("Players").LocalPlayer
+local mouse = player:GetMouse()
+
+local event = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+
+mouse.KeyDown:Connect(function(keybind)
+	if _G.Toxic == false then
+		if keybind == key then
+			event:FireServer(words[math.random(#words)], "All")
+		end
+	end
+end)
