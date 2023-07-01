4217e4d27facf33c20a7e37da569e24e8e85ae3f
x9o <95175948+x9o@users.noreply.github.com>
2023-07-01 11:54:25 +0800

    Create meme.lua

---
+++ meme.lua
@@ -0,0 +1,21 @@
+local key = "u" -- the toxic key
+_G.Toxic = false
+
+local words = 
+	{
+		"I will spread your cheeks lil bro 👐😭🙏",
+		"I better not see you on the streets lil bro 😭🙏",
+  }
+
+local player = game:GetService("Players").LocalPlayer
+local mouse = player:GetMouse()
+
+local event = game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents"):WaitForChild("SayMessageRequest")
+
+mouse.KeyDown:Connect(function(keybind)
+	if _G.Toxic == false then
+		if keybind == key then
+			event:FireServer(words[math.random(#words)], "All")
+		end
+	end
+end)
